cb2ad103800ee92ce26d96bfaebefeb2ec746426
Jesse1205 <Jeece1205@hotmail.com>
2016-09-12 18:01:07 +0800

    更新remote方法

--- Middleware/GameServer/Script/CsScript/Remote/ServerToServer.cs
+++ Middleware/GameServer/Script/CsScript/Remote/ServerToServer.cs
@@ -6,12 +6,10 @@ namespace Game.Remote
 {
     class ServerToServer
     {
-        private readonly static object syncObject;
         private readonly static RemoteService tcpRemote;
         static ServerToServer()
         {
             tcpRemote = RemoteService.CreateTcpProxy("0", "192.168.0.100", 9001, 30 * 1000);
-            syncObject = new object();
         }
 
         public static int Action1000(int userId)
@@ -20,15 +18,12 @@ namespace Game.Remote
             var param = new RequestParam();
             param.Add("UserId", userId);
             AutoResetEvent waitHandle = new AutoResetEvent(false);
-            lock (syncObject)
-            {
-                tcpRemote.Call("Battle1000", param, package =>
-                  {
-                      var reader = new MessageStructure(package.Message as byte[]);
-                      result = reader.ReadInt();
-                      waitHandle.Set();
-                  });
-            }
+            tcpRemote.Call("Battle1000", param, package =>
+                {
+                    var reader = new MessageStructure(package.Message as byte[]);
+                    result = reader.ReadInt();
+                    waitHandle.Set();
+                });
             waitHandle.WaitOne(5000);
             return result;
         }
